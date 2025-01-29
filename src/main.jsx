715e8dc00028437b70de8a1a6a2a1a6c4dcc7ef2
Rahul Parmar <119917030+rahulprmr18@users.noreply.github.com>
2025-01-29 23:12:57 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+export const backend = "https://portfolio-backend-vgi8.onrender.com";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
